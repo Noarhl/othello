db5f47c1d234f91965197c55c02f14a4ea491a71
Noarhl <kai.t20051230@gmail.com>
2024-04-27 04:36:11 +0900

    Lv1END

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,8 @@ const directions = [
   [1, -1],
   [-1, -1],
   [-1, 0],
-  [-1, -1],
-  [-1, 0],
+  [0, -1],
+  [-1, 1],
 ];
 
 const Home = () => {
@@ -22,58 +22,60 @@ const Home = () => {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const anothercolor = (x: number, y: number) => {
+    return board[y][x] === 2 / turnColor;
+  };
+
+  const samecolor = (x: number, y: number) => {
+    return board[y][x] === turnColor;
+  };
+
+  const uncolor = (x: number, y: number) => {
+    return board[y][x] === 0;
+  };
 
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     console.log(turnColor);
     const newBoard = structuredClone(board);
 
-    for (const direction of directions) {
-      const dx = direction[0];
-      const dy = direction[1];
+    if (newValid(x, y) && uncolor(x, y)) {
+      for (const direction of directions) {
+        const dx = direction[0];
+        const dy = direction[1];
 
-      for (let n: number = 1; n <= 6; n++) {
-        if (newValid(x + dx * n, y + dy * n) && anothercolor(x + dx * n, y + dy * n)) {
-          if (
-            newValid(x + dx * (n + 1), y + dy * (n + 1)) &&
-            samecolor(x + dx * (n + 1), y + dy * (n + 1))
-          ) {
-            for (let m = 1; m <= n; m++) {
-              newBoard[y][x] = turnColor;
-              newBoard[y + dy * m][x + dx * m] = turnColor;
-              setTurnColor(2 / turnColor);
+        for (let n: number = 1; n <= 6; n++) {
+          if (newValid(x + dx * n, y + dy * n) && anothercolor(x + dx * n, y + dy * n)) {
+            if (
+              newValid(x + dx * (n + 1), y + dy * (n + 1)) &&
+              samecolor(x + dx * (n + 1), y + dy * (n + 1))
+            ) {
+              for (let m = 1; m <= n; m++) {
+                newBoard[y][x] = turnColor;
+                newBoard[y + dy * m][x + dx * m] = turnColor;
+                setTurnColor(2 / turnColor);
+              }
+              break;
             }
+          }
+          if (newValid(x + dx * n, y + dy * n) && samecolor(x + dx * n, y + dy * n)) {
             break;
           }
-        }
-        if (newValid(x + dx * n, y + dy * n) && samecolor(x + dx * n, y + dy * n)) {
-          break;
-        }
-        if (newValid(x + dx * n, y + dy * n) && uncolor(x + dx * n, y + dy * n)) {
-          break;
+          if (newValid(x + dx * n, y + dy * n) && uncolor(x + dx * n, y + dy * n)) {
+            break;
+          }
         }
       }
     }
     setBoard(newBoard);
-
-    const anothercolor = (x: number, y: number) => {
-      return board[y][x] === 2 / turnColor;
-    };
-
-    const samecolor = (x: number, y: number) => {
-      return board[y][x] === turnColor;
-    };
-
-    const uncolor = (x: number, y: number) => {
-      return board[y][x] === 0;
-    };
   };
 
   return (
@@ -82,11 +84,14 @@ const Home = () => {
         {board.map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              {color !== 0 && (
+              {color !== 0 && color !== 3 && (
                 <div
                   className={styles.stone}
                   style={{ background: color === 1 ? '#000' : '#FFF' }}
                 />
+              )}
+              {color === 3 && (
+                <div className={styles.condidate} style={{ background: '#CAE5CD' }} />
               )}
             </div>
           )),
@@ -96,3 +101,5 @@ const Home = () => {
   );
 };
 export default Home;
+
+<div className={styles.scoreboard} />;
